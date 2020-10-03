df1b45534cccb47460935e2ce6ae28a430eb84e9
Lucan F <dapps911@hotmail.com>
2020-10-03 15:07:24 -0400

    update user GET & POST route to res.josn()

--- routes/users.js
+++ routes/users.js
@@ -5,64 +5,62 @@ const User = require('../models/user');
 
 router.get('/', async (req, res, next) => {
   try {
-    console.log(await User.findAll());
-    res.status(200);
+    const users = await User.findAll();
+    res.json(users).status(200);
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
 });
 
 router.post('/', async (req, res, next) => {
+  const { firstName, lastName, emailAddress, password } = req.body;
   try {
-    const { firstName, lastName, emailAddress, password } = req.body;
-    await User.create({
+    const user = await User.create({
       firstName,
       lastName,
       emailAddress,
       password,
     });
     res.location('/');
-    res.status(201);
-    // res.json({
-    //   message: 'Well Done!!',
-    // });
+    res.json(user).status(201);
+    // console.log({ user });
   } catch (error) {
     console.error('Unable to create user!', error);
   }
 });
 
 module.exports = router;
-const express = require('express');
-const router = express.Router();
-const sequelize = require('../db');
-const User = require('../models/user');
+// const express = require('express');
+// const router = express.Router();
+// const sequelize = require('../db');
+// const User = require('../models/user');
 
-router.get('/', async (req, res, next) => {
-  try {
-    console.log(await User.findAll());
-    res.status(200);
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-});
+// router.get('/', async (req, res, next) => {
+//   try {
+//     console.log(await User.findAll());
+//     res.status(200);
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// });
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { firstName, lastName, emailAddress, password } = req.body;
-    await User.create({
-      firstName,
-      lastName,
-      emailAddress,
-      password,
-    });
-    res.location('/');
-    res.status(201);
-    // res.json({
-    //   message: 'Well Done!!',
-    // });
-  } catch (error) {
-    console.error('Unable to create user!', error);
-  }
-});
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const { firstName, lastName, emailAddress, password } = req.body;
+//     await User.create({
+//       firstName,
+//       lastName,
+//       emailAddress,
+//       password,
+//     });
+//     res.location('/');
+//     res.status(201);
+//     // res.json({
+//     //   message: 'Well Done!!',
+//     // });
+//   } catch (error) {
+//     console.error('Unable to create user!', error);
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
